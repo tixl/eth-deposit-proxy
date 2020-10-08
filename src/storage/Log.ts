82d01ed160c4d860138f6510d07d055afd2e50a8
Boris Kolar <boris.kolar.mail@gmail.com>
2020-10-08 13:27:08 +0200

    betted data encoding/decoding system

--- src/storage/Log.ts
+++ src/storage/Log.ts
@@ -1,12 +1,11 @@
 import { assert, unsigned, bytes } from "../core/Core"
-import Reader from "../data/Reader"
-import Writer from "../data/Writer"
+import Data from "../data/Data"
 import Store from "./Store"
 
 export default class Log {
     static async create(store: Store): Promise<Log> {
         let t = await store.get(bytes())
-        return new Log(store, t.length ? Reader.unsigned(t).value : 0)
+        return new Log(store, t.length ? Data.unsigned.decode(t).value : 0)
     }
 
     constructor(store: Store, size: int) {
@@ -20,12 +19,12 @@ export default class Log {
 
     async at(index: int): Promise<Bytes> {
         assert(unsigned(index) < this.size)
-        return await this._store.get(Writer.unsigned(index))
+        return await this._store.get(Data.unsigned.encode(index))
     }
 
     async add(value: Bytes): Promise<void> {
-        await this._store.set(Writer.unsigned(this.size), value)
-        let t = Writer.unsigned(this.size + 1)
+        await this._store.set(Data.unsigned.encode(this.size), value)
+        let t = Data.unsigned.encode(this.size + 1)
         await this._store.set(t)
         this._size++
     }
